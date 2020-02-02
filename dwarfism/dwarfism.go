7746a6ce11e69e706884890048c8443d52f3067e
gido5731 <gido5731@gido-desk-deb-v2.imagen.click>
2020-02-02 05:35:43 -0500

    Added support for requesting URLs

--- dwarfism/dwarfism.go
+++ dwarfism/dwarfism.go
@@ -25,7 +25,8 @@ type userInfo struct {
 }
 
 // ShortPage is a function to return the homepage of ShortPage
-func ShortPage(resp http.ResponseWriter, req *http.Request, config tools.Config) {
+// outputURL should be in html <a> tags
+func ShortPage(resp http.ResponseWriter, req *http.Request, config tools.Config, outputURL string) {
 	pageTemplate := template.New("short page templated.")
 	content, err := ioutil.ReadFile("server/selector/modules/dwarfism-2.0/shortPage.html")
 	page := string(content)
@@ -44,7 +45,8 @@ func ShortPage(resp http.ResponseWriter, req *http.Request, config tools.Config)
 	//fmt.Println(field)
 	tData := tools.TData{
 		config.RecaptchaPubKey,
-		config.URLPrefix}
+		config.URLPrefix,
+		outputURL}
 	//upload(resp, req)
 	//log.Traceln("Form data: ", field, "\ntData: ", tData)
 	err = pageTemplate.Execute(resp, tData)
@@ -55,6 +57,39 @@ func ShortPage(resp http.ResponseWriter, req *http.Request, config tools.Config)
 	}
 }
 
+// Biggify redirects the user to the larger url
+func Biggify(resp http.ResponseWriter, req *http.Request, config tools.Config, url string) {
+	if url == "" {
+		http.Redirect(resp, req, "/dwarfism2.0", 301)
+	}
+
+	db, err := sql.Open("mysql", fmt.Sprintf("%s@tcp(127.0.0.1:3306)/ImgSrvr", config.SQLAcc))
+	if err != nil {
+		log.Error("Oh noez, could not connect to database")
+		tools.ErrorHandler(resp, req, 500, "I dont know what I'm doing")
+		return
+	}
+	log.Debug("Oi, mysql did thing")
+	defer db.Close()
+
+	var longURL string
+	err = db.QueryRow("SELECT origURL FROM shortlinks WHERE shortURL=?", url).Scan(&longURL)
+	switch {
+	case err == sql.ErrNoRows:
+		tools.ErrorHandler(resp, req, 404, "idk maybe check for typos?")
+	case err != nil:
+		tools.ErrorHandler(resp, req, 500, "no clue, but it probably was your fault")
+		return
+	default:
+	}
+
+	if !strings.HasPrefix(longURL, "http") {
+		longURL = "http://" + longURL
+	}
+
+	http.Redirect(resp, req, longURL, 301)
+}
+
 // ShortResp responds to the form from ShortPage
 func ShortResp(resp http.ResponseWriter, req *http.Request, config tools.Config) {
 	req.ParseForm()
@@ -63,16 +98,28 @@ func ShortResp(resp http.ResponseWriter, req *http.Request, config tools.Config)
 	)
 	isLoggedIn, _ := sessions.Verify(resp, req, config.SQLAcc, &username)
 
+	longURL = req.FormValue("lURL")
+	if longURL == "" {
+		tools.ErrorHandler(resp, req, 400, "You need to  specify a url!")
+		return
+	}
+
+	if !strings.HasPrefix(longURL, "http") {
+		longURL = "http://" + longURL
+	}
+
 	db, err := sql.Open("mysql", fmt.Sprintf("%s@tcp(127.0.0.1:3306)/ImgSrvr", config.SQLAcc))
 	if err != nil {
 		log.Error("Oh noez, could not connect to database")
+		tools.ErrorHandler(resp, req, 500, "I dont know what I'm doing")
 		return
 	}
 	log.Debug("Oi, mysql did thing")
 	defer db.Close()
 
 	if isLoggedIn {
-		err = db.QueryRow("SELECT FROM shortlinks WHERE short = ?", req.FormValue("sURL")).Scan()
+		err = db.QueryRow("SELECT * FROM shortlinks WHERE shortURL = ?", req.FormValue("sURL")).Scan()
+		//log.Traceln(err)
 		if err == sql.ErrNoRows {
 			shortURL = req.FormValue("sURL")
 		} else {
@@ -90,15 +137,15 @@ func ShortResp(resp http.ResponseWriter, req *http.Request, config tools.Config)
 		}
 	}
 
-	longURL = req.FormValue("lURL")
-
 	err = Shorten(longURL, shortURL, db, userInfo{username, req.RemoteAddr})
 	if err != nil {
 		tools.ErrorHandler(resp, req, 500, "I'm pressing the buttons but it's not goin' anywhere!")
 		log.Errorln("Error while shortening: ", err)
 	}
 
-	http.Redirect(resp, req, "/d/"+shortURL, http.StatusSeeOther)
+	fullShortURL := config.BaseURL + config.URLPrefix + "d/" + shortURL
+
+	ShortPage(resp, req, config, fullShortURL)
 }
 
 // Shorten is a function to add a shortURL to the DB
@@ -108,10 +155,6 @@ func Shorten(origURL, shortURL string, db *sql.DB, user userInfo) error {
 	}
 
 	_, err := db.Exec("INSERT INTO shortlinks VALUES(?, ?, ?, ?)", origURL, shortURL, user.username, user.ipAddr)
-
-	if err != nil {
-
-	}
 
 	return err
 }
